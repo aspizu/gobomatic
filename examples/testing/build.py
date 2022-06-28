31ac5e144113d728050b835b3bee85e9d33bad69
xAspirus <movxa@protonmail.com>
2022-06-29 00:51:55 +0530

    Added tasks.txt for tasks

---
+++ examples/testing/build.py
@@ -0,0 +1,9 @@
+from gobomatic import *
+
+from main import Self as main
+
+stage = Sprite("Stage", costumes=["assets/blank.svg"])
+
+Self = Project(sprites=[stage, main])
+
+Self.export("project.sb3")
---
+++ examples/testing/main.py
@@ -0,0 +1,5 @@
+# fmt: off
+
+from gobomatic import *
+
+Self = Sprite(name=__name__, costumes=["assets/blank.svg"])
