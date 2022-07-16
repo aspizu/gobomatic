0b47fac608cbe069fcd9d4019a67c4610ad2fd70
aspizu@protonmail.com <movxa@protonmail.com>
2022-07-16 20:43:21 +0530

    add warp= for funcs

--- tools/sb3dump.py
+++ tools/sb3dump.py
@@ -8,6 +8,6 @@ project_file = zipfile.ZipFile(sys.argv[1])
 project_json = json.loads(project_file.read("project.json"))
 
 
-blocks = project_json["targets"][1]["blocks"]
+blocks = project_json["targets"][1]
 
 print(blocks)
